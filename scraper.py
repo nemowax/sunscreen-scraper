9b6bf7794e1d0c3c27e4475de02e2d5c58082d7e
Nemo Wax <nemo.waxx@gmail.com>
2023-05-24 18:03:39 -0700

    scrape_product_page() works with offline or online html

--- scraper.py
+++ scraper.py
@@ -7,11 +7,11 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 
 
-# TODO: Include homosalate percentage
+# TODO: Include homosalate percentage!!
 # TODO: pull ingredients ONLY from ingredients area
 # TODO: refactor for redability, Python naming conventions and comment conventions
 # TODO: revise where webdriver is opened and closed by best coding practices
-
+"""
 # SETUP CHROME TO RUN IN HEADLESS MODE
 # Configure Chrome options to run in headless mode
 options = webdriver.ChromeOptions()
@@ -21,6 +21,7 @@ options.add_argument('--disable-dev-shm-usage')
 
 # Create a Chrome WebDriver instance
 driver = webdriver.Chrome(options=options)
+"""
 
 # Retrieves page source code for dynamic web pages
 def get_page_source(url):
@@ -29,9 +30,10 @@ def get_page_source(url):
 
     # Access hidden URLs and elements
     # Example: Click on a "Show More" button and wait for the new content to load
-    #show_more_button = driver.find_element_by_id('show-more-button')
+    #show_more_button = driver.find_element_by_id('manual_load_button') # ERROR
     #show_more_button.click()
 
+    """
     # Scroll to the bottom of the dynamic webpage
     # Get the initial height of the page
     prev_height = driver.execute_script("return document.body.scrollHeight")
@@ -42,11 +44,14 @@ def get_page_source(url):
         driver.implicitly_wait(10) # seconds
         # Get the new height of the page
         new_height = driver.execute_script("return document.body.scrollHeight")
+        print(prev_height)
+        print(new_height)
         # Break the loop if the page height remains unchanged (reached the bottom)
         if prev_height == new_height:
             break
         # Update the previous height
         prev_height = new_height
+    """
 
     # Retrieve the updated page source with the hidden content
     html_content = driver.page_source
@@ -54,23 +59,37 @@ def get_page_source(url):
 
 # download the HTML document with an HTTP GET request
 # !!! Depreciated with Selenium - for static use only.
-response = requests.get("https://well.ca/searchresult.html?keyword=sunscreen")
+#response = requests.get("https://well.ca/searchresult.html?keyword=sunscreen")
+
 #response = requests.get("https://well.ca/products/shiseido-clear-sunscreen-stick-spf_276474.html")
 #response = requests.get("https://jackheatonlive.com/")
 
 # parse the HTML content of the page listing all products
-page_source = get_page_source("https://well.ca/searchresult.html?keyword=sunscreen")
-soup = BeautifulSoup(page_source, "html.parser")
+#page_source = get_page_source("https://well.ca/searchresult.html?keyword=sunscreen")
+#soup = BeautifulSoup(page_source, "html.parser")
 #soup = BeautifulSoup(response.content, "html.parser") # !!! Depreciated with Selenium - for static use only.
 
-# Scrape all individual sunscreen product webpage urls from main page.
-product_elements = soup.find_all(class_="grid__item col-md-3 col-sm-4 col-xs-6") #returns a list of elements
 urls = []
-for product_element in product_elements:
-    url = product_element.find("a")["href"]
-    urls.append(url)
+#JENKY CODE FOR FAST download SCRAPING
+def jenky_scrape(web_url):
+    response = requests.get(web_url)
+    soup = BeautifulSoup(response.content, "html.parser")
 
-print(len(urls))
+    # Scrape all individual sunscreen product webpage urls from main page.
+    product_elements = soup.find_all(class_="grid__item col-md-3 col-sm-4 col-xs-6") #returns a list of elements
+
+    for product_element in product_elements:
+        url = product_element.find("a")["href"]
+        urls.append(url)
+
+#jenky_scrape("https://well.ca/searchresult.html?keyword=sunscreen")
+#jenky_scrape("https://well.ca/index.php?main_page=advanced_search_result&keyword=sunscreen&page=0")
+#jenky_scrape("https://well.ca/index.php?main_page=advanced_search_result&keyword=sunscreen&page=1")
+#jenky_scrape("https://well.ca/index.php?main_page=advanced_search_result&keyword=sunscreen&page=2")
+
+#print(len(urls))
+#print(urls[-1])
+
 # This function works for static pages ONLY
 def download_html(url, filename):
     response = requests.get(url)
@@ -81,6 +100,7 @@ def download_html(url, filename):
         print(f"HTML from {url} saved as {filename}")
     else:
         print(f"Failed to download HTML from {url}. Status code: {response.status_code}")
+
 
 """
 # Create a subfolder to save the files
@@ -126,6 +146,7 @@ sunscreen_products.append(category_headings)
 
 
 # EXPORT TO FILE AS CSV
+# TODO: rewrite as "write_products_to_file()
 def writeToFile():
     # create the "products.csv" file
     csv_file = open('products.csv', 'w', encoding='utf-8', newline='')
@@ -141,11 +162,20 @@ def writeToFile():
     # release the file resources
     csv_file.close()
 
-def scrape_product_page(url):
+
+def scrape_product_page(url="", file_path=""):
     #response = requests.get("https://well.ca/products/shiseido-clear-sunscreen-stick-spf_276474.html")
-    response = requests.get(url)
-    # parse the HTML content of the page listing specific product
-    soup = BeautifulSoup(response.content, "html.parser")
+    if url != "":
+        response = requests.get(url)
+        # parse the HTML content of the page listing specific product
+        soup = BeautifulSoup(response.content, "html.parser")
+    elif file_path != "":
+        with open(file_path, 'r', encoding='utf-8') as file:
+            html_content = file.read() # html content
+        soup = BeautifulSoup(html_content, "html.parser")
+    else:
+        print("No page found.")
+        return
 
     # Get the first reference of the word "homosalate" on the page
     #TODO: case handling of ingreidents like Homosalate.
@@ -170,6 +200,7 @@ def scrape_product_page(url):
     # Check to see if key term returns an empty list. aka no references found.
     if homosalate:
         print("yes true")
+        # Find all the paragraphs containing the word "homosalate"
         homosalate_flag = True
         # I don't need to refind the string containing homosalate using soup. I need to extract the string, then parse that string using regex to slice out the substring contatining the perecentage.
         homosalate_percent = soup(string=re.compile('homosalate', re.IGNORECASE))
@@ -220,7 +251,7 @@ def scrape_product_page(url):
     writeToFile() # writes all to file every pass. Consider refactoring.
 
 
-scrape_product_page("https://well.ca/products/shiseido-clear-sunscreen-stick-spf_276474.html")
+#scrape_product_page("https://well.ca/products/shiseido-clear-sunscreen-stick-spf_276474.html")
 
 # SCRAPE DATA FROM ALL SUNSCREEN PAGES
 # Iterates over only the first 20 elements.
@@ -229,6 +260,25 @@ for url in urls[:20]:
     scrape_product_page(url)
     print("scraped")
     """
+
+# TEST DATA SCRAP FROM OFFLINE
+html_file_path = os.path.join('html_files', "html_0.html")
+scrape_product_page(file_path=html_file_path)
+html_file_path = os.path.join('html_files', "html_2.html")
+scrape_product_page(file_path=html_file_path)
+
+# SCRAPE DATA FROM ALL OFFLINE SUNSCREEN PAGES
+# Continues until no more files found.
+folder_path = 'html_files'
+try:
+    # Iterate through all files in the folder
+    for file_name in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file_name)
+        if os.path.isfile(file_path):
+            scrape_product_page(file_path=file_path)
+except FileNotFoundError:
+    print("No files found in this folder.")
+
 
 
 #ERROR HANDLING
@@ -273,7 +323,7 @@ else:
 """
 
 # Quit the WebDriver
-driver.quit()
+#driver.quit()
 
 
 
